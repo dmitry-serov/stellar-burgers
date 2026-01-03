abe2bbf0e3ab7ab90be5b72cce715a7a16a305c7
dmitry-serov <frickmen@yandex.ru>
2026-01-03 16:11:11 +0300

    Добавить редьюсер для управления заказами, включая создание заказа и получение заказа по номеру; обновить компоненты для использования новых селекторов и асинхронных действий

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,20 +1,51 @@
 import { FC, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { TConstructorIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
-import { useSelector } from '../../services/store';
-import { selectConstructorItems } from '../../services/slices/constructorSlice';
+import { useSelector, useDispatch } from '../../services/store';
+import {
+  selectConstructorItems,
+  clearConstructor
+} from '../../services/slices/constructorSlice';
+import {
+  createOrder,
+  clearOrderModalData,
+  selectOrderRequest,
+  selectOrderModalData
+} from '../../services/slices/orderSlice';
 
 export const BurgerConstructor: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const constructorItems = useSelector(selectConstructorItems);
-
-  const orderRequest = false;
-
-  const orderModalData = null;
+  const orderRequest = useSelector(selectOrderRequest);
+  const orderModalData = useSelector(selectOrderModalData);
 
   const onOrderClick = () => {
     if (!constructorItems.bun || orderRequest) return;
+
+    // TODO: добавить проверку авторизации после создания userSlice
+    // if (!isAuthenticated) {
+    //   navigate('/login');
+    //   return;
+    // }
+
+    const ingredientIds = [
+      constructorItems.bun._id,
+      ...constructorItems.ingredients.map((item) => item._id),
+      constructorItems.bun._id
+    ];
+
+    dispatch(createOrder(ingredientIds))
+      .unwrap()
+      .then(() => {
+        dispatch(clearConstructor());
+      });
   };
-  const closeOrderModal = () => {};
+
+  const closeOrderModal = () => {
+    dispatch(clearOrderModalData());
+  };
 
   const price = useMemo(
     () =>
--- src/services/store.ts
+++ src/services/store.ts
@@ -8,8 +8,13 @@ import {
 
 import { ingredientsSlice } from './slices/ingredientsSlice';
 import { constructorSlice } from './slices/constructorSlice';
+import { orderSlice } from './slices/orderSlice';
 
-const rootReducer = combineSlices(ingredientsSlice, constructorSlice);
+const rootReducer = combineSlices(
+  ingredientsSlice,
+  constructorSlice,
+  orderSlice
+);
 
 const store = configureStore({
   reducer: rootReducer,
